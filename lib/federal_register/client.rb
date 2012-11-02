2418d46112e97d2daebcaf91dcfd0e736fdb4d6f
Andrew Carpenter <andrew@criticaljuncture.org>
2012-11-02 12:41:52 -0700

    improve error handling

--- lib/federal_register/client.rb
+++ lib/federal_register/client.rb
@@ -1,8 +1,19 @@
 class FederalRegister::Client
   include HTTParty
   
-  class RecordNotFound < HTTParty::ResponseError; end
-  class ServerError < HTTParty::ResponseError; end
+  class ResponseError < HTTParty::ResponseError
+    def message
+      response.body
+    end
+
+    def to_s
+      message
+    end
+  end
+
+  class RecordNotFound < ResponseError; end
+  class BadRequest < ResponseError; end
+  class ServerError < ResponseError; end
   
   base_uri 'http://api.federalregister.gov/v1'
   
@@ -12,6 +23,8 @@ class FederalRegister::Client
     case response.code
     when 200
       response
+    when 400
+      raise BadRequest.new(response)
     when 404
       raise RecordNotFound.new(response)
     when 500
