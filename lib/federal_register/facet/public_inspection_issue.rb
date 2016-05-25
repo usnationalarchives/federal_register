b0e6c4aab3f238a68c540ef96254dbc500d8e7d4
Peregrinator <bob.burbach@gmail.com>
2016-05-25 11:43:12 -0700

    Pass the conditions onto the subclasses that we are creating so that we can ask the facet for its search conditions and get back the proper set of conditions.

--- lib/federal_register/facet/public_inspection_issue.rb
+++ lib/federal_register/facet/public_inspection_issue.rb
@@ -8,11 +8,21 @@ class FederalRegister::Facet::PublicInspectionIssue < FederalRegister::Base
   end
 
   def special_filings
-    @special_filings ||= filing_class.new(attributes['special_filings'])
+    @special_filings ||= filing_class.new(
+      attributes['special_filings'],
+      conditions.deep_merge({
+        conditions: {special_filing: 1}
+      })
+    )
   end
 
   def regular_filings
-    @regular_filings ||= filing_class.new(attributes['regular_filings'])
+    @regular_filings ||= filing_class.new(
+      attributes['regular_filings'],
+      conditions.deep_merge({
+        conditions: {special_filing: 0}
+      })
+    )
   end
 
   def self.search(args={})
--- lib/federal_register/facet/public_inspection_issue/type_filing.rb
+++ lib/federal_register/facet/public_inspection_issue/type_filing.rb
@@ -1,17 +1,22 @@
 class FederalRegister::Facet::PublicInspectionIssue::TypeFiling < FederalRegister::Facet::PublicInspectionIssue
-  attr_reader :document_types
+  attr_reader :document_types, :search_conditions
 
-  def initialize(attributes, options={})
-    @document_types = attributes.map{|k,v| DocumentTypeFacet.new(v)}
+  def initialize(attributes, conditions, options={})
+    @search_conditions = conditions
+    @document_types = attributes.map{|k,v| DocumentTypeFacet.new(k, v, @search_conditions)}
   end
 
   private
 
   class DocumentTypeFacet
-    attr_reader :count, :name
-    def initialize(attributes)
+    attr_reader :count, :name, :search_conditions
+
+    def initialize(type, attributes, search_conditions)
       @count = attributes['count']
       @name = attributes['name']
+      @search_conditions = search_conditions.deep_merge({
+        conditions: {type: type}
+      })
     end
   end
 end
